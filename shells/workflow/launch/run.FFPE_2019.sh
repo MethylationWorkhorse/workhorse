728001d504650b3b472f2418481ad4d6dad55aea
Bret D. Barnes <bbarnes@illumina.com>
2019-12-02 11:57:22 -0800

    Added some internal test scripts.

---
+++ shells/workflow/launch/run.FFPE_2019.sh
@@ -0,0 +1,27 @@
+
+idatName=FFPE_2019
+prgmTag=workhorse
+
+TOP_MAC=/Users/bbarnes/Documents/Projects/${prgmTag}/git/${prgmTag}
+TOP_LIX=/illumina/scratch/darkmatter/Projects/${prgmTag}/git/${prgmTag}
+
+if [ -e ${TOP_MAC} ]; then
+    TOP=${TOP_MAC}
+    CONDA=mac
+    RSCRIPT=Rscript
+elif [ -e ${TOP_LIX} ]; then
+    TOP=${TOP_LIX}
+    CONDA=conda_4.6.8
+    # CONDA=Anaconda2-2019.10-Linux-x86_64
+    # CONDA=Anaconda3-2019.10-Linux-x86_64
+    RSCRIPT=/illumina/scratch/darkmatter/thirdparty/${CONDA}/bin/Rscript
+else
+    echo "Unrecognized top directory!"
+    exit
+fi
+
+EXE=${TOP}/shells/workflow/run.${prgmTag}.sh
+
+${EXE} ${idatName}
+
+## End of file
